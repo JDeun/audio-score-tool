ee17186809546996b76997a5eabc38892eb06130
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-06 17:57:25 +0900

    feat: mount product song workspace shell

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import YoutubeImport from "./YoutubeImport";
+import ProductRoot from "./ProductRoot";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <YoutubeImport />
-    <App />
+    <ProductRoot />
   </StrictMode>,
 );
