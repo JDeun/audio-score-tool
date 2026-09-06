8a1e2039e4db8b5de627770660196f887b76f85e
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-06 21:04:34 +0900

    feat: wrap desktop app with error recovery boundary

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AppErrorBoundary from "./AppErrorBoundary";
 import ProductRoot from "./ProductRoot";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ProductRoot />
+    <AppErrorBoundary>
+      <ProductRoot />
+    </AppErrorBoundary>
   </StrictMode>,
 );
